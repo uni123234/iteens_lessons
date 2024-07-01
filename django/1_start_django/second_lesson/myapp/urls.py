0408bb5ca6f12da99e5eb05ca946d89076886947
uni123234 <uni2.0zxc@gmail.com>
2024-07-01 08:22:15 +0200

    edit students

--- django/1_start_django/second_lesson/myapp/urls.py
+++ django/1_start_django/second_lesson/myapp/urls.py
@@ -1,9 +1,11 @@
+# urls.py
 from django.urls import path
 from . import views
 
-app_name = 'myapp'
+app_name = "myapp"
 
 urlpatterns = [
-    path('', views.student_list, name='student_list'),
-    path('create_students/', views.create_students, name='create_students'),
+    path("", views.student_list, name="student_list"),
+    path("create/", views.create_students, name="create_students"),
+    path("edit/<int:student_id>/", views.edit_students, name="edit_students"),
 ]
--- django/1_start_django/second_lesson/myapp/views.py
+++ django/1_start_django/second_lesson/myapp/views.py
@@ -1,18 +1,33 @@
-from django.shortcuts import render, redirect
+# views.py
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import School, Class, Student
 from .forms import StudentForm
 
+
 def student_list(request):
     students = Student.objects.all()
-    return render(request, 'index.html', {'students': students})
+    return render(request, "index.html", {"students": students})
+
 
 def create_students(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('myapp:student_list')
+            return redirect("myapp:student_list")
     else:
         form = StudentForm()
-    return render(request, 'create_students.html', {'form': form})
+    return render(request, "create_students.html", {"form": form})
+
+
+def edit_students(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("myapp:student_list")
+    else:
+        form = StudentForm(instance=student)
+    return render(request, "edit_students.html", {"form": form})
